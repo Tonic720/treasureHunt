919122c724a1f3ff05cb766cf228c230f2872e94
Olivia Hu <olivia@172-16-31-89.DYNAPOOL.NYU.EDU>
2016-07-18 11:47:21 -0400

    first commit

---
+++ Assets/Scripts/PlayerPhysicsMove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPhysicsMove : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Update is where rendering and input update
+	// FixedUpdate is called once per physics frame
+	void FixedUpdate () {
+		// reset velocity every frame
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+
+		// press W to apply upward force
+		if ( Input.GetKey(KeyCode.UpArrow) ) {
+			GetComponent<Rigidbody2D>().velocity += new Vector2(0f, moveSpeed) *Time.deltaTime;
+		}
+
+		if ( Input.GetKey(KeyCode.LeftArrow) ) {
+			GetComponent<Rigidbody2D>().velocity += new Vector2(-moveSpeed, 0f) *Time.deltaTime;
+		}
+
+		if ( Input.GetKey(KeyCode.RightArrow) ) {
+			GetComponent<Rigidbody2D>().velocity += new Vector2(moveSpeed, 0f) *Time.deltaTime;
+		}
+
+		if ( Input.GetKey(KeyCode.DownArrow) ) {
+			GetComponent<Rigidbody2D>().velocity += new Vector2(0f, -moveSpeed) *Time.deltaTime;
+		}
+
+		if (transform.position.x <= -40f) {
+			transform.position = new Vector2(-40f, transform.position.y);
+		} else if (transform.position.x >= 40f) {
+			transform.position = new Vector2(40f, transform.position.y);
+		}
+
+		// Y axis
+		if (transform.position.y <= -40f) {
+			transform.position = new Vector2(transform.position.x, -40f);
+		} else if (transform.position.y >= 40f) {
+			transform.position = new Vector2(transform.position.x, 40f);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Security.Cryptography.X509Certificates;
+using System;
+
+public class Treasure : MonoBehaviour {
+
+	bool didPlayerWin = false;
+	public Text gameText;
+	public Transform player;
+	public Transform maletrainer;
+	public Transform zubat;
+	public Transform femaletrainer;
+	public Transform doduo;
+	public Transform lake;
+	public Transform charmander;
+
+	void Update () {
+		if ((player.position - maletrainer.transform.position).magnitude < 5f) {
+			gameText.text = "Eevee: Oh no, there's a trainer! He must be here to catch mother! I'd better go the other way!";
+		} else if ((player.position - zubat.transform.position).magnitude < 1f) {
+			gameText.text = "Zubat: I just want to befriend humans but all they do is run away from me ... oh Vaporeon? She's hiding north of here.";
+		} else if ((player.position - femaletrainer.transform.position).magnitude < 5f) {
+			gameText.text = "Eevee: Ahh another one of them! They are everywhere! I have to go the other way!";
+		} else if ((player.position - doduo.transform.position).magnitude < 1f) {
+			gameText.text = "Doduo: Why doesn't anybody want to catch me? Vaporeon? Haven't seen her anywhere near here.";
+		} else if ((player.position - lake.transform.position).magnitude < 5f) {
+			gameText.text = "Eevee: Water! Mother likes water. She must be around here somewhere!";
+		} else if ((player.position - charmander.transform.position).magnitude < 1f) {
+			gameText.text = "Charmander: I'm staying far away from the hoard of trainers and you should too. Vaporeon is that way!";
+		} else if ((player.position - transform.position).magnitude < 1f) {
+			gameText.text = "Press [space] to reunite with your mother.";
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				didPlayerWin = true;
+			} else if (didPlayerWin) {
+				gameText.text = "You found Vaporeon before the New Yorkers did!";
+			}
+		}
+
+	}
+}
